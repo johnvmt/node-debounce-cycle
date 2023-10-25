144e1a3b7a977098ccd6a6f24e2632b59acdf198
John Murphy-Teixidor <johnvmt@gmail.com>
2023-10-24 20:32:45 -0700

    v0.0.1 - Working state

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # node-debounce-cycle
 Run a function on a set interval, with possibility for early runs
+
+## Warnings
+
+There is very little error-checking. Use at your own peril.
+
+## Options
+
+* `min`: minimum time between starts in milliseconds, or a function that returns milliseconds
+* `max`: maximum time between starts in milliseconds, or a function that returns milliseconds. Used when cycling.
+* `retry`: minimum time between starts in milliseconds when the run function returns an error, or a function that returns milliseconds
+
+## Examples
+
+```
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const run = async () => {
+    const startDate = new Date();
+    await sleep(500);
+    return startDate;
+}
+
+const controller = new DebounceCycle(run, {
+    min: 1000, // runs must start at least 1,000ms (1s) apart, except in retry
+    max: 3000, // when cycling, start every 3,000ms (3s)
+    retry: 500, // when run returns an error, retry after 500ms
+    start: false, // don't start cycling
+    immediate: false // wait for at least min or max (depending on which is requested) on the first run
+});
+
+(async () => {
+    const result1 = controller.request(); // requested with min
+    const result2 = await controller.request(); // also requested with min
+    console.log("Result 2", result2);
+})();
+```
---
+++ src/DebounceCycle.js
@@ -0,0 +1,309 @@
+class DebounceCycle {
+    constructor(callback, options = {}) {
+
+        this._callback = callback;
+
+        this._options = {
+            immediate: true,
+            start: true,
+            ...options
+        };
+
+        // TODO validate max, min and retry
+        this._max = this._options.max;
+        this._min = this._options.min;
+        this._retry = this._options.retry;
+
+        this._requestPromises = new Map();
+
+        this._status = DebounceCycle.STATUSES.STOPPED;
+
+        if(this._options.start) // start automatically
+            this.start();
+    }
+
+    get status() {
+        return this._status;
+    }
+
+    /**
+     * sleep time before retrying, if attempt fails
+     */
+    get retry() {
+        if(typeof this._retry === "function")
+            return this._retry();
+        else
+            return this._retry;
+    }
+
+    /**
+     * Minimum time in ms between start of cycles
+     */
+    get min() {
+        if(typeof this._min === "function")
+            return this._min();
+        else
+            return this._min;
+    }
+
+    /**
+     * Maximum time in ms between start of cycles, if cycling is in effect
+     */
+    get max() {
+        if(typeof this._max === "function")
+            return this._max();
+        else
+            return this._max;
+    }
+
+    /**
+     * Minimum time in ms between start of cycles
+     * If value is a function, it will be called each time module.min is accessed
+     */
+    set min(msOrFunction) {
+        if(msOrFunction !== this._min) {
+            this._min = msOrFunction;
+
+            if(this.status === DebounceCycle.STATUSES.SLEEPING && this._nextRunRequestSource === DebounceCycle.RUNREQUESTTYPE.MIN) {
+                clearTimeout(this._sleepTimeout);
+                delete this._nextRunStartTime;
+                this._status = DebounceCycle.STATUSES.STOPPED;
+                this._requestRun(DebounceCycle.RUNREQUESTTYPE.MIN);
+            }
+        }
+    }
+
+    /**
+     * Maximum time in ms between start of cycles
+     * If value is a function, it will be called each time module.min is accessed
+     */
+    set max(msOrFunction) {
+        if(msOrFunction !== this._min) {
+            this._min = msOrFunction;
+
+            if(this.status === DebounceCycle.STATUSES.SLEEPING && this._nextRunRequestSource === DebounceCycle.RUNREQUESTTYPE.MAX) {
+                clearTimeout(this._sleepTimeout);
+                delete this._nextRunStartTime;
+                this._status = DebounceCycle.STATUSES.STOPPED;
+                this._requestRun(DebounceCycle.RUNREQUESTTYPE.MAX);
+            }
+        }
+    }
+
+    /**
+     * Maximum time in ms between start of cycles, if an attempt fails
+     * If value is a function, it will be called each time module.min is accessed
+     */
+    set retry(msOrFunction) {
+        if(msOrFunction !== this._retry) {
+            this._retry = msOrFunction;
+
+            if(this.status === DebounceCycle.STATUSES.SLEEPING && this._nextRunRequestSource === DebounceCycle.RUNREQUESTTYPE.RETRY) {
+                clearTimeout(this._sleepTimeout);
+                delete this._nextRunStartTime;
+                this._status = DebounceCycle.STATUSES.STOPPED;
+                this._requestRun(DebounceCycle.RUNREQUESTTYPE.RETRY);
+            }
+        }
+    }
+
+    /**
+     * Start cycling (calling run function every max ms)
+     */
+    start() {
+        this._cycling = true;
+        this._requestRun(DebounceCycle.RUNREQUESTTYPE.MAX);
+    }
+
+    /**
+     * Stop cycling (calling run function every max ms)
+     */
+    stop() {
+        this._cycling = false;
+
+        // currently awaiting a run
+        if(this.status === DebounceCycle.STATUSES.SLEEPING && this._nextRunRequestSource === DebounceCycle.RUNREQUESTTYPE.MAX) {
+            clearTimeout(this._sleepTimeout);
+            this._status = DebounceCycle.STATUSES.STOPPED;
+        }
+    }
+
+    request() {
+        console.log("REQUEST!!");
+        let resolvePromise;
+        let rejectPromise;
+
+        const promise = new Promise((resolve, reject) => {
+            resolvePromise = resolve;
+            rejectPromise = reject;
+        });
+
+        this._requestPromises.set(promise, {resolve: resolvePromise, reject: rejectPromise});
+
+        this._requestRun(DebounceCycle.RUNREQUESTTYPE.MIN);
+
+        return promise;
+    }
+
+    _sleepTimeFromRequestType(runRequestType) {
+        if(runRequestType === DebounceCycle.RUNREQUESTTYPE.MAX)
+            return this.max;
+        else if(runRequestType === DebounceCycle.RUNREQUESTTYPE.MIN)
+            return this.min;
+        else if(runRequestType === DebounceCycle.RUNREQUESTTYPE.RETRY)
+            return this.retry;
+        else
+            throw new Error(`Unknown request source`);
+    }
+
+    /**
+     * Returns milliseconds until next run can start, based on the request type and previous runs
+     * @param {*} runRequestType
+     * @returns
+     */
+    _timeToNextRunStart(runRequestType) {
+        if(!this._lastRunStartTime && this._options.immediate) // no previous runs and should run immediately
+            return 0;
+        else {
+            const sleepTime = this._sleepTimeFromRequestType(runRequestType);
+
+            if(!this._lastRunStartTime) // no previous run (and no immediate)
+                return sleepTime;
+            else // has previously run
+                return Math.max(0, (this._lastRunStartTime + sleepTime) - Date.now());
+        }
+    }
+
+    _requestRun(runRequestType) {
+        this._log('debug', `Got request to run from source: ${runRequestType}`);
+
+        if(this.status !== DebounceCycle.STATUSES.RUNNING && this._nextRunRequestSource !== runRequestType) { // not already running and not the same request source (request source sleep times can change)
+            this._log('debug', `Not already running, and new run request source is different from currently-sleeping source (if any)`);
+            const timeToRequestedNextRunStartTime = this._timeToNextRunStart(runRequestType); // get requested ms to next start, based on request method and previous run
+
+            // if currently sleeping, check if time to next start
+            if(timeToRequestedNextRunStartTime > 0) { // would need to sleep for some time before next start
+                this._log('debug', `Sleep time > 0. Compare to currently-sleeping (if any) to determine which should be honored.`);
+                const requestedNextRunStartTime = Date.now() + timeToRequestedNextRunStartTime; // calculate time next run would start, given the request
+
+                if(!this._nextRunStartTime || requestedNextRunStartTime < this._nextRunStartTime) { // no next run set, or requested run would start before currently-sleeping request
+                    this._log('debug', `New request will start sooner than previous request (${requestedNextRunStartTime} vs ${this._nextRunStartTime}). Sleep for ${timeToRequestedNextRunStartTime}`);
+
+                    // if currently sleeping, clear timeout of previously-set sleep
+                    if(this._sleepTimeout)
+                        clearTimeout(this._sleepTimeout);
+
+                    // set up new sleep timer
+                    this._nextRunStartTime = Date.now() + timeToRequestedNextRunStartTime; // target time for next run to start
+                    this._nextRunRequestSource = runRequestType;
+                    this._status = DebounceCycle.STATUSES.SLEEPING;
+
+                    this._sleepTimeout = setTimeout(() => {
+                        this._log('debug', `Finished sleeping`);
+                        // clean up vars set only for sleeping
+                        delete this._nextRunRequestSource;
+                        delete this._nextRunStartTime;
+
+                        this._run();
+                    }, timeToRequestedNextRunStartTime);
+                }
+                else
+                    this._log('debug', `Previous request will start sooner than new request. Discarding new request.`);
+            }
+            else {
+                this._log('debug', `Sleep time is 0. Run immediately.`);
+                delete this._nextRunRequestSource;
+                delete this._nextRunStartTime;
+                this._run();
+            }
+        }
+        else
+            this._log('debug', `Conditions not met to request new run. Skipping.`)
+
+        // check if request source is the same as the current one?
+
+        // change to min, if it's not already so it won't be stopped
+
+    }
+
+    /**
+     * Resolve all requestor promises with callback success result
+     * @param {*} result
+     */
+    _pushCallbackSuccess(result) {
+        const requestPromises = this._requestPromises;
+        this._requestPromises = new Map(); // reset the map for the next request
+
+        for(let {resolve} of requestPromises.values()) {
+            try {
+                resolve(result);
+            }
+            catch(error) {
+                // TODO log this somehow
+            }
+        }
+    }
+
+    /**
+     * Reject all requestor promises with callback error
+     * @param {*} error
+     */
+    _pushCallbackError(error) {
+        const requestPromises = this._requestPromises;
+        this._requestPromises = new Map(); // reset the map for the next request
+
+        for(let {reject} of requestPromises.values()) {
+            try {
+                reject(error);
+            }
+            catch(error) {
+                this._log(error.message);
+            }
+        }
+    }
+
+    /**
+     * Run the callback, and optionally start the next cycle
+     * @returns {Promise<void>}
+     * @private
+     */
+    async _run() {
+        this._status = DebounceCycle.STATUSES.RUNNING;
+        this._lastRunStartTime = Date.now();
+        try {
+            const result = await this._callback();
+            this._pushCallbackSuccess(result);
+
+            if(this._cycling)
+                this._requestRun(DebounceCycle.RUNREQUESTTYPE.MAX); // cycle
+            else {
+                this._status = DebounceCycle.STATUSES.STOPPED;
+                this._log('debug', `Run completed and not cycling. Stopping.`);
+            }
+        }
+        catch(error) {
+            // retry
+            this._pushCallbackError(error);
+            this._requestRun(DebounceCycle.RUNREQUESTTYPE.RETRY);
+        }
+    }
+
+    _log(level, ...messageParts) {
+        if(this._options.logger)
+            this._options.logger[level](messageParts.join(" "));
+    }
+
+    static RUNREQUESTTYPE = Object.freeze({
+        MIN: "MIN",
+        MAX: "MAX",
+        RETRY: "RETRY"
+    });
+
+    static STATUSES = Object.freeze({
+        STOPPED: "STOPPED",
+        SLEEPING: "SLEEPING",
+        RUNNING: "RUNNING"
+    });
+}
+
+export default DebounceCycle;
